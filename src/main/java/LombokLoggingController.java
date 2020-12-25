ba6702f87fe359866b35856d2075b26fa9d7eb4b
Alan Tan <alanwuha91@gmail.com>
2020-12-25 15:28:40 +0000

    feat: add log4j annotation using lombok dependency

---
+++ src/main/java/LombokLoggingController.java
@@ -0,0 +1,21 @@
+import org.springframework.boot.*;
+import org.springframework.web.bind.annotation.*;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@Log4j2
+public class LombokLoggingController {
+    
+    @RequestMapping("/lombok")
+    public String index() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("A INFO Message");
+        log.warn("A WARN Message");
+        log.error("A ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+
+}
+
